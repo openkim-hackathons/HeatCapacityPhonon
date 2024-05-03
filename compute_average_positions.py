095b6eab08b9f42a637ba11d6f0a7d880e55cd5d
Guanming <guanming2718@gmail.com>
2024-05-03 13:49:15 +0000

    output the description part above the table

--- compute_average_positions.py
+++ compute_average_positions.py
@@ -1,13 +1,14 @@
 import numpy as np
 import os
 import sys
+import re
 '''
 Notes: run this file calculate the averaged position over files(e.g average_position_step100.dump,average_position_step200.dump)
 if run "python compute_average_position.py dir_path"
-average_positionXXX.dump files will be loaded from dir_path
+average_positionXXX.dupm files will be loaded from dir_path
 and the output is writen to dir_path/average_position_over_files.ouput
 '''
-id_pos_dict = {} # the dictionary in the form id:position
+
 def get_id_pos_dict(file_name:str) -> dict:
     '''
     input: 
@@ -18,9 +19,10 @@ def get_id_pos_dict(file_name:str) -> dict:
     '''
     id_pos_dict = {}
     header4N = ["NUMBER OF ATOMS"]
-    header4pos = ["id","type","f_avePos[1]","f_avePos[2]","f_avePos[3]"]
+    header4pos = ["id","f_avePos[1]","f_avePos[2]","f_avePos[3]"]
     is_table_started = False
     is_natom_read = False
+    
     with open(file_name,"r") as f:
         line = f.readline()
         count_content_line = 0
@@ -38,7 +40,8 @@ def get_id_pos_dict(file_name:str) -> dict:
                 count_content_line += 1        
                 words = line.split()
                 id = int(words[0])
-                pos = np.array([float(words[2]),float(words[3]),float(words[4])])
+                #pos = np.array([float(words[2]),float(words[3]),float(words[4])])
+                pos = np.array([float(words[1]),float(words[2]),float(words[3])])
                 id_pos_dict[id] = pos 
             if count_content_line > 0 and count_content_line >= N:
                 break
@@ -59,6 +62,7 @@ if __name__ == "__main__":
     
     # extract and store all the data
     pos_list = []
+    max_step,last_step_file = -1, ""
     for file_name in os.listdir(data_dir):
         if ("average_position" in file_name) and ("dump" in file_name):
             file_path = os.path.join(data_dir,file_name)
@@ -66,19 +70,38 @@ if __name__ == "__main__":
             id_pos = sorted(id_pos_dict.items())
             id_list = [pair[0] for pair in id_pos]
             pos_list.append([pair[1] for pair in id_pos])
+            # check if this is the last step
+            step = int(re.findall(r'\d+', file_name)[-1])
+            if step > max_step:
+                last_step_file,max_step = os.path.join(data_dir ,file_name),step 
     pos_arr = np.array(pos_list)
     avg_pos = np.mean(pos_arr,axis=0)
+    # get the lines above the table from the file of the last step
+    with open(last_step_file,"r") as f:
+        header4pos = ["id","f_avePos[1]","f_avePos[2]","f_avePos[3]"]
+        line = f.readline()
+        description_str = ""
+        is_table_started = False
+        while line:
+            description_str += line
+            is_table_started = np.all([flag in line for flag in header4pos])
+            if is_table_started:
+                break
+            else:
+                line = f.readline()
+
     # write the output to the file
     output_file = os.path.join(data_dir,"average_position_over_files.out")
     with open(output_file,"w") as f:
-        header_str = "id  pos[1]  pos[2]  pos[3]"
-        f.write(header_str)
+        f.write(description_str)
+        #header_str = "id  pos[1]  pos[2]  pos[3]"
+        #f.write(header_str)
         for i in range(len(id_list)):
-            f.write("\n")
             f.write(str(id_list[i]))
             f.write("  ")
             for dim in range(3):
                 f.write('{:3.6}'.format(avg_pos[i,dim]))
                 f.write("  ")
+            f.write("\n")
 
         
