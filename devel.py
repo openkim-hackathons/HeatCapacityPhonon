88dcbf159eea86f76646c9147e6b799bd12c5734
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2024-05-07 19:52:33 +0000

    Changed to new base class

--- devel.py
+++ devel.py
@@ -6,62 +6,13 @@ import numpy as np
 import numpy.typing as npt
 from ase import Atoms
 from ase.build import bulk
-from ase.utils import structure_comparator as sc
-from kim_python_utils.ase import CrystalGenomeTest
+from kim_test_utils.test_driver import CrystalGenomeTestDriver
 
 
-class HeatCapacityPhonon(CrystalGenomeTest):
-    def reduce_and_avg(self, atoms, repeat):
-        '''
-        Function to reduce all atoms to the original unit cell position.
-
-        @param atoms : repeated atoms object
-        @param repeat : repeat tuple
-        '''
-        cell = atoms.get_cell()
-        
-        # Divide each unit vector by its number of repeats.
-        # See https://stackoverflow.com/questions/19602187/numpy-divide-each-row-by-a-vector-element.
-        cell = cell / np.array(repeat)[:, None]
-
-        # Decrease size of cell in the atoms object.
-        atoms.set_cell(cell)
-        atoms.set_pbc((True, True, True))
-
-        # Set averaging factor
-        M = np.prod(repeat)
-
-        # Wrap back the repeated atoms on top of the reference atoms in the original unit cell.
-        positions = atoms.get_positions(wrap=True)
-        
-        number_atoms = len(atoms)
-        original_number_atoms = number_atoms // M
-        assert number_atoms == original_number_atoms * M
-        positions_in_prim_cell = np.zeros((original_number_atoms, 3))
-
-        # Start from end of the atoms because we will remove all atoms except the reference ones.
-        for i in reversed(range(number_atoms)):
-            if i >= original_number_atoms:
-                # Get the distance to the reference atom in the original unit cell with the 
-                # minimum image convention.
-                distance = atoms.get_distance(i % original_number_atoms, i,
-                                              mic=True, vector=True)
-                # Get the position that has the closest distance to the reference atom in the 
-                # original unit cell.
-                position_i = positions[i % original_number_atoms] + distance
-                # Remove atom from atoms object.
-                atoms.pop()
-            else:
-                # Atom was part of the original unit cell.
-                position_i = positions[i]
-            # Average.
-            positions_in_prim_cell[i % original_number_atoms] += position_i / M
-
-        atoms.set_positions(positions_in_prim_cell)
-        
-    def _calculate(self, structure_index: int, temperature: float, pressure: float, timestep: float, 
+class HeatCapacityPhonon(CrystalGenomeTestDriver):
+    def _calculate(self, temperature: float, pressure: float, timestep: float, 
                    number_sampling_timesteps: int, repeat: Tuple[int, int, int] = (3, 3, 3), 
-                   seed: Optional[int] = None) -> None:
+                   seed: Optional[int] = None, loose_triclinic_and_monoclinic=False, **kwargs) -> None:
         """
         structure_index:
             KIM tests can loop over multiple structures (i.e. crystals, molecules, etc.). 
@@ -86,34 +37,28 @@ class HeatCapacityPhonon(CrystalGenomeTest):
 
         # TODO: Check all arguments.
         
-        # TODO: Ask whether this is the correct way.
-        # TODO: Filter out repeated species.
-        # TODO: Just get the species as a test argument (they are definitely alphabetically ordered).
-        # TODO: They changed the atoms object so this might actually be fixed.
-        # Get species and masses of the atoms.
-        atoms = self.atoms[structure_index]
-        
-        # TODO: Remove this hack at some point.
-        species_of_each_atom = atoms.get_chemical_symbols()[:1]
-        masses = atoms.get_masses()
-        
         # Copy original atoms so that their information does not get lost when the new atoms are modified.
-        atoms_new = atoms.copy()
+        atoms_new = self.atoms.copy()
         
         # UNCOMMENT THIS TO TEST A TRICLINIC STRUCTURE!
         # atoms_new = bulk('Ar', 'fcc', a=5.248)
+
+        # This is how ASE obtains the species that are written to the initial configuration.
+        # These species are passed to kim interactions.
+        # See https://wiki.fysik.dtu.dk/ase/_modules/ase/io/lammpsdata.html#write_lammps_data
+        symbols = atoms_new.get_chemical_symbols()
+        species = sorted(set(symbols))
         
+        # Build supercell.
         atoms_new = atoms_new.repeat(repeat)
 
         # Write lammps file.
         TDdirectory = os.path.dirname(os.path.realpath(__file__))
         structure_file = os.path.join(TDdirectory, "output/zero_temperature_crystal.lmp")
-        atoms_new.write(structure_file, format="lammps-data")
-        self._add_masses_to_structure_file(structure_file, masses)
-
-        # LAMMPS for heat capacity
+        atoms_new.write(structure_file, format="lammps-data", masses=True)
+        
+        # Get random 31-bit unsigned integer.
         if seed is None:
-            # Get random 31-bit unsigned integer.
             seed = random.getrandbits(31)
             
         # TODO: Move damping factors to argument.
@@ -122,7 +67,7 @@ class HeatCapacityPhonon(CrystalGenomeTest):
 
         # Run NPT simulation.
         variables = {
-            "modelname": self.model_name,
+            "modelname": self.kim_model_name,
             "temperature": temperature,
             "temperature_seed": seed,
             "temperature_damping": tdamp,
@@ -130,15 +75,16 @@ class HeatCapacityPhonon(CrystalGenomeTest):
             "pressure_damping": pdamp,
             "timestep": timestep,
             "number_sampling_timesteps": number_sampling_timesteps,
-            "species": " ".join(species_of_each_atom) 
+            "species": " ".join(species) 
         }
         # TODO: Possibly run MPI version of Lammps if available.
         command = (
             "lammps " 
             + " ".join(f"-var {key} '{item}'" for key, item in variables.items()) 
             + " -in npt_equilibration.lammps")
-        # TODO: Play around with number of independent samples in kim-convergence.
         subprocess.run(command, check=True, shell=True)
+
+        # TODO: Remove subprocess call in this function.
         self._extract_and_plot(("v_vol_metal", "v_temp_metal"))
         
         # TODO: Guanming changes this into a function call and also removes the average_position.dump.* files.
@@ -152,7 +98,8 @@ class HeatCapacityPhonon(CrystalGenomeTest):
         self.reduce_and_avg(atoms_new, repeat)
 
         # AFLOW Symmetry check
-        self._update_aflow_designation_from_atoms(structure_index, atoms_new)
+        self._get_crystal_genome_designation_from_atoms_and_verify_unchanged_symmetry(
+            atoms_new, loose_triclinic_and_monoclinic=loose_triclinic_and_monoclinic)
         
         """
         # NVT simulation
@@ -221,18 +168,54 @@ class HeatCapacityPhonon(CrystalGenomeTest):
         # PROPERTY WRITING END
         ####################################################
         """
-    
-    @staticmethod
-    def _add_masses_to_structure_file(structure_file: str, masses: Iterable[float]) -> None:
-        # TODO: This does not always work... (especially when cube is included in atoms)
-        with open(structure_file, "a") as file:
-            print(file=file)
-            print("Masses", file=file)
-            print(file=file)
-            for i, mass in enumerate(masses):
-                print(f"    {i+1} {mass}", file=file)
-                # TODO: Remove this hack at some point.
-                break
+
+    def reduce_and_avg(self, atoms, repeat):
+        '''
+        Function to reduce all atoms to the original unit cell position.
+
+        @param atoms : repeated atoms object
+        @param repeat : repeat tuple
+        '''
+        cell = atoms.get_cell()
+        
+        # Divide each unit vector by its number of repeats.
+        # See https://stackoverflow.com/questions/19602187/numpy-divide-each-row-by-a-vector-element.
+        cell = cell / np.array(repeat)[:, None]
+
+        # Decrease size of cell in the atoms object.
+        atoms.set_cell(cell)
+        atoms.set_pbc((True, True, True))
+
+        # Set averaging factor
+        M = np.prod(repeat)
+
+        # Wrap back the repeated atoms on top of the reference atoms in the original unit cell.
+        positions = atoms.get_positions(wrap=True)
+        
+        number_atoms = len(atoms)
+        original_number_atoms = number_atoms // M
+        assert number_atoms == original_number_atoms * M
+        positions_in_prim_cell = np.zeros((original_number_atoms, 3))
+
+        # Start from end of the atoms because we will remove all atoms except the reference ones.
+        for i in reversed(range(number_atoms)):
+            if i >= original_number_atoms:
+                # Get the distance to the reference atom in the original unit cell with the 
+                # minimum image convention.
+                distance = atoms.get_distance(i % original_number_atoms, i,
+                                              mic=True, vector=True)
+                # Get the position that has the closest distance to the reference atom in the 
+                # original unit cell.
+                position_i = positions[i % original_number_atoms] + distance
+                # Remove atom from atoms object.
+                atoms.pop()
+            else:
+                # Atom was part of the original unit cell.
+                position_i = positions[i]
+            # Average.
+            positions_in_prim_cell[i % original_number_atoms] += position_i / M
+
+        atoms.set_positions(positions_in_prim_cell)
     
     @staticmethod
     def _extract_and_plot(property_names: Iterable[str]) -> None:
@@ -320,11 +303,11 @@ if __name__ == "__main__":
     atoms = bulk("AlCo", "cesiumchloride", a=2.8663, cubic=True)
     model_name = "EAM_Dynamo_VailheFarkas_1997_CoAl__MO_284963179498_005"
 
-    atoms = bulk("Ar", "fcc", a=5.248, cubic=True)
+    atoms = bulk("Ar", "fcc", a=5.248)
     model_name = "LJ_Shifted_Bernardes_1958MedCutoff_Ar__MO_126566794224_004"
 
     subprocess.run(f"kimitems install {model_name}", shell=True, check=True)
 
-    test = HeatCapacityPhonon(model_name=model_name, atoms=atoms)
-    test(temperature = 50.0, pressure = 1.0, timestep=0.001, number_sampling_timesteps=10, 
-         repeat=(10, 10, 10))
+    test_driver = HeatCapacityPhonon(model_name)
+    test_driver(atoms, temperature = 50.0, pressure = 1.0, timestep=0.001, number_sampling_timesteps=10, 
+                repeat=(2, 2, 2), loose_triclinic_and_monoclinic=False)
