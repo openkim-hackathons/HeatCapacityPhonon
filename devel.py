Fixed sign issue

devel.py
@@ -496,8 +496,7 @@ class HeatCapacityPhonon(CrystalGenomeTestDriver):
         # Error computation
         dx = H_plus_err
         dy = H_minus_err
-        c_err = np.sqrt(((dx / (2 * eps)) ** 2) + (- (dy / (2 * eps)) ** 2))
-
+        c_err = np.sqrt(((dx / (2 * eps)) ** 2) + ((dy / (2 * eps)) ** 2))
         # Return
         return c, c_err
 