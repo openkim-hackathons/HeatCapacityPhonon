b199fcf19fc707f914b8e9d498f50e24c03b162e
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2024-07-24 03:05:37 +0000

    Temporary fix for zero mean error

--- run_length_control.py
+++ run_length_control.py
@@ -1,7 +1,7 @@
 """Run length control for LAMMPS."""
 
 import numpy as np
-from typing import Optional
+from typing import Optional, Sequence
 from lammps import lammps
 import kim_convergence as cr
 
@@ -22,8 +22,9 @@ MINIMUM_NUMBER_OF_INDEPENDENT_SAMPLES: Optional[int] = 1000
 # for the ratio of halfwidth to sample mean. If n_variables > 1,
 # relative_accuracy can be a scalar to be used for all variables or a 1darray
 # of values of size n_variables.
-RELATIVE_ACCURACY: float = 0.01
-ABSOLUTE_ACCURACY: Optional[float] = None
+# TODO: This should only be used for presumably cubic simulation boxes.
+RELATIVE_ACCURACY: Sequence[Optional[float]] = [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, None, None, None]
+ABSOLUTE_ACCURACY: Sequence[Optional[float]] = [None, None, None, None, None, None, 0.01, 0.01, 0.01]
 # Probability (or confidence interval) and must be between 0.0 and 1.0, and
 # represents the confidence for calculation of relative halfwidths estimation.
 CONFIDENCE: float = 0.95
