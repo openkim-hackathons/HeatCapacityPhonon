e16f093d5591fd28c2ba7ca944a61d717336845d
ilia Nikiforov <nikif002@umn.edu>
2026-05-17 14:52:34 -0500

    update rlc_min_samples to minimum required to reliably run quartz with vashishta NPTCrystalstructure

--- test_driver/test_driver.py
+++ test_driver/test_driver.py
@@ -27,7 +27,7 @@ class TestDriver(SingleCrystalTestDriver):
         random_seeds: Optional[Sequence[int]] = (1, 2, 3),
         rlc_n_every: int = 10,
         rlc_initial_run_length: int = 10000,
-        rlc_min_samples: int = 100,
+        rlc_min_samples: int = 400,
         output_dir: str = "output",
         equilibration_plots: bool = True,
         temperature_step_fraction: float = 0.01,
